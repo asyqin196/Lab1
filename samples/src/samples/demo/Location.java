e9a47023a288c3b48f76ff72826167565ec9b050
asyqin196 <asyqinhariss@gmail.com>
2022-03-10 16:03:09 +0800

    First settergetter

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,14 @@
+package samples.demo;
+
+public class Location {
+
+	private int assignment;
+
+	public int getAssignment() {
+		return assignment;
+	}
+
+	public void setAssignment(int assignment) {
+		this.assignment = assignment;
+	}
+}
